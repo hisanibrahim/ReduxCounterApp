261dafe92b0fd27dbd20f67106c3659bd7837bc8
Hisan <hisan@creatnlrn.com>
2019-07-30 13:01:57 +0530

    CounterApp seperated from App class

--- App.js
+++ App.js
@@ -1,32 +1,12 @@
 import React, { Component } from 'react';
 import { Text, View, TouchableOpacity } from 'react-native';
+import CounterApp from './src/CounterApp';
 
 export default class App extends Component {
 
-  state = {
-    counter: 0
-  }
-
-  increseCounter() {
-    this.setState({ counter: this.state.counter + 1 })
-  }
-  decreseCounter() {
-    this.setState({ counter: this.state.counter - 1 })
-  }
-
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <View style={{ flexDirection: 'row', justifyContent: "space-around", width: 250 }}>
-          <TouchableOpacity onPress={() => this.increseCounter()}>
-            <Text style={{ fontSize: 20 }}>Increase</Text>
-          </TouchableOpacity>
-          <Text style={{ fontSize: 20 }}>{this.state.counter}</Text>
-          <TouchableOpacity onPress={() => this.decreseCounter()}>
-            <Text style={{ fontSize: 20 }}>Decrease</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
+      <CounterApp></CounterApp>
     );
   }
 }
---
+++ src/CounterApp.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+
+export default class CounterApp extends Component {
+
+    state = {
+        counter: 0
+    }
+
+    increseCounter() {
+        this.setState({ counter: this.state.counter + 1 })
+    }
+    decreseCounter() {
+        this.setState({ counter: this.state.counter - 1 })
+    }
+
+    render() {
+        return (
+            <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+                <View style={{ flexDirection: 'row', justifyContent: "space-around", width: 250 }}>
+                    <TouchableOpacity onPress={() => this.increseCounter()}>
+                        <Text style={{ fontSize: 20 }}>Increase</Text>
+                    </TouchableOpacity>
+                    <Text style={{ fontSize: 20 }}>{this.state.counter}</Text>
+                    <TouchableOpacity onPress={() => this.decreseCounter()}>
+                        <Text style={{ fontSize: 20 }}>Decrease</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        );
+    }
+}
