2a442848af1206628691ec14beaeee75375113ef
Hisan <hisan@creatnlrn.com>
2019-07-30 16:34:28 +0530

    Redux implemented in the app

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
 import CounterApp from './src/CounterApp';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+/**
+ * Store - holds our state - THERE IS ONLY ONE STATE 
+ * Action - State can be modified using actions - SIMPLE OBJECTS 
+ * Dispatcher - Action needs to be sent by someone - known as dispatching an action
+ * Reducer - receives the action and modifies the state to give us a new state 
+ *    - pure functions 
+ *    - only mandatory argument is the 'type' 
+ * Subscriber - listens for state change to update the ui  
+ */
+
+const initialState = {
+  counter: 0
+}
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'INCREASE_COUNTER':
+      return { counter: state.counter + 1 }
+    case 'DECREASE_COUNTER':
+      return { counter: state.counter - 1 }
+  }
+  return state;
+}
+
+const store = createStore(reducer);
 
 export default class App extends Component {
 
+
   render() {
     return (
-      <CounterApp></CounterApp>
+      <Provider store={store}>
+        <CounterApp />
+      </Provider>
     );
   }
 }
--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,28 +1,19 @@
 import React, { Component } from 'react';
 import { Text, View, TouchableOpacity } from 'react-native';
+import { connect } from 'react-redux'
 
-export default class CounterApp extends Component {
 
-    state = {
-        counter: 0
-    }
-
-    increseCounter() {
-        this.setState({ counter: this.state.counter + 1 })
-    }
-    decreseCounter() {
-        this.setState({ counter: this.state.counter - 1 })
-    }
+class CounterApp extends Component {
 
     render() {
         return (
             <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
                 <View style={{ flexDirection: 'row', justifyContent: "space-around", width: 250 }}>
-                    <TouchableOpacity onPress={() => this.increseCounter()}>
+                    <TouchableOpacity onPress={() => this.props.increseCounter()}>
                         <Text style={{ fontSize: 20 }}>Increase</Text>
                     </TouchableOpacity>
-                    <Text style={{ fontSize: 20 }}>{this.state.counter}</Text>
-                    <TouchableOpacity onPress={() => this.decreseCounter()}>
+                    <Text style={{ fontSize: 20 }}>{this.props.counter}</Text>
+                    <TouchableOpacity onPress={() => this.props.decreseCounter()}>
                         <Text style={{ fontSize: 20 }}>Decrease</Text>
                     </TouchableOpacity>
                 </View>
@@ -30,3 +21,18 @@ export default class CounterApp extends Component {
         );
     }
 }
+
+function mapStateToProps(state) {
+    return {
+        counter: state.counter
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        increseCounter: () => dispatch({ type: 'INCREASE_COUNTER' }),
+        decreseCounter: () => dispatch({ type: 'DECREASE_COUNTER' })
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CounterApp);
